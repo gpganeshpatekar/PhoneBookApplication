421fa017e2c7ce29617989ef9b6c8281fd7ed6ca
gpganeshpatekar <gpganeshpatekar@gmail.com>
2022-08-23 18:39:14 +0530

    PBA-7 : declare methods in the service interface and provide an implementation in the service impl class

--- src/main/java/com/demo/phonebookapp/services/ContactServicesI.java
+++ src/main/java/com/demo/phonebookapp/services/ContactServicesI.java
@@ -1,5 +1,15 @@
 package com.demo.phonebookapp.services;
 
+import java.util.List;
+
+import com.demo.phonebookapp.entities.Contact;
+
 public interface ContactServicesI {
+	
+	  boolean  saveContact(Contact contact);
+	  List<Contact>  getAllContact();
+	  Contact    getContactById(Integer contactId);
+	  Contact updateContact(Contact contact,Integer contactId);
+	  boolean deleteContactById(Integer contactId);
 
 }
--- src/main/java/com/demo/phonebookapp/services/impl/ContactServicesImpl.java
+++ src/main/java/com/demo/phonebookapp/services/impl/ContactServicesImpl.java
@@ -1,11 +1,49 @@
 package com.demo.phonebookapp.services.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.demo.phonebookapp.entities.Contact;
+import com.demo.phonebookapp.repositories.ContactRepository;
 import com.demo.phonebookapp.services.ContactServicesI;
 
 
 @Service
 public class ContactServicesImpl implements ContactServicesI {
+	
+	@Autowired
+	private ContactRepository contactRepository;
+
+	@Override
+	public boolean saveContact(Contact contact) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Contact> getAllContact() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contact getContactById(Integer contactId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contact updateContact(Contact contact, Integer contactId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteContactById(Integer contactId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
